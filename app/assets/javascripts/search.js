3c053214ca5b3fea6421e63b64ca83ff0985e3e9
Michael Stewart <michael_stewart+gitdev@vt.edu>
2012-08-17 18:06:51 -0400

    enable undo sort

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -8,6 +8,9 @@ $(document).ready(function() {
   function enableResultEvents () {
     $('.search .results .result-div .content').mouseenter(searchResultMouseEnter);
     clickifyResultsAndAnnotations();
+    $(".result-div").each(function(){
+      $(this).attr("original-order", $(this).attr("order"));
+    });
   }
 	// When a result-div's content is moused-over, hide all other check or delete
  	// images and show only the ones for this result
@@ -76,16 +79,20 @@ $(document).ready(function() {
     
   }
 
-  $(window).scroll(function() {
-    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-     	if (!locked) {
-     		$.ajax({url: '/search/ajax', data: "uri="+escape($('.next-page').text()), dataType: "html", success: loadMore});
+  function enableScroll() {
+    $(window).scroll(function() {
+      if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+       	if (!locked) {
+       		$.ajax({url: '/search/ajax', data: "uri="+escape($('.next-page').text()), dataType: "html", success: loadMoreResults});
+       	}
      	}
-   	}
-   });
+     });
+  }
+
+  enableScroll();
 
   // Function that handles the ajax response to load more results
-  function loadMore (data) {
+  function loadMoreResults (data) {
     $('.next-page').remove();
     $('.results').append(data);
     var addtlResults = $('.ajax-load-wrapper').detach();
@@ -105,6 +112,54 @@ $(document).ready(function() {
   $('.ajax-load').ajaxStop(function() {
     locked = false;
     $(this).hide();
+  });
+
+  $("#rearrange").click(function(){
+    //supposedly we want the checked items, then the unmodified items, then the items with check and x
+    //then the items with x (after rearrangement)
+
+    console.log("rearranging");
+    if ($(this).text() == "CritiSORT!") {
+      $(this).text("Undo CritiSort");
+      //constant to be added for checked
+      var checkedAddend = -200;
+
+      // constant to be added for unchecked
+      var deletededAddend = 400;
+
+      $(".result-div.checked-result").each(function(){
+        $(this).attr("order", parseInt($(this).attr("order")) + checkedAddend);
+      });
+
+      $(".result-div.deleted-result").each(function(){
+        $(this).attr("order", parseInt($(this).attr("order")) + deletededAddend);
+      }); 
+
+      var results = $(".result-div").sort(function(a,b) {
+        return $(a).attr("order") - $(b).attr("order");
+      });
+
+      $(".result-div").detach();
+
+      $(".ajax-load-wrapper").before(results);
+
+      //turn off load more on scroll
+      $(window).unbind("scroll");
+    }
+    else {
+      $(this).text("CritiSORT!"); 
+
+      var results = $(".result-div").sort(function(a,b) {
+        return $(a).attr("original-order") - $(b).attr("original-order");
+      });
+
+      $(".result-div").detach();
+
+      $(".ajax-load-wrapper").before(results);
+
+      enableScroll();
+
+    }
   });
 
   enableResultEvents();
