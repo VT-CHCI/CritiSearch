67d3dbf0ff252a2dccff47247329c78c73b993a9
Michael Stewart <michael_stewart+gitdev@vt.edu>
2012-10-16 11:36:02 -0400

    pile of changes to try to coax heroku into playing ball

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'font-awesome-rails'
 
 # Gems used only for assets and not required
 # in production environments by default.
-group :assets do
+# group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
 
@@ -28,7 +28,7 @@ group :assets do
   
 
   gem 'uglifier', '>= 1.0.3'
-end
+# end
 
 # Use mysql2 gem in production environment
 group :production do
--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,9 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module Platypus
@@ -62,5 +62,7 @@ module Platypus
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    config.assets.precompile += ['main.css']
   end
 end
